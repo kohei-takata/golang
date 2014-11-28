612c9009ec451bcfc96813fcbb506675c300c94f
tako-black <gennarishock+tako@gmail.com>
2014-11-28 23:45:22 +0900

    29

--- 25/main.go
+++ 25/main.go
@@ -3,17 +3,12 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
 
-var (
-	p = Vertex{1, 2}
-	q = &Vertex{1, 2}
-	r = Vertex{X: 1}
-	s = Vertex{}
-)
-
 func main() {
-	fmt.Println(p, q, r, s)
+	v := new(Vertex)
+	fmt.Println(v)
+	v.X, v.Y = 11, 9
+	fmt.Println(v)
 }
