2fa2fd72e1a950382283ba3d6e9326e5adfb8479
tako-black <gennarishock+tako@gmail.com>
2014-12-19 08:09:50 +0900

    66

--- 66/main.go
+++ 66/main.go
@@ -4,20 +4,27 @@ import (
 	"fmt"
 )
 
-func fibonacci(n int, c chan int) {
+func fibonacci(c, quit chan int) {
 	x, y := 0, 1
-	for i := 0; i < 10; i++ {
-		c <- x
-		x, y = y, x+y
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
 	}
-	close(c)
-
 }
 
 func main() {
 	c := make(chan int, 10)
-	go fibonacci(cap(c), c)
-	for i := range c {
-		fmt.Println(i)
-	}
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
 }
